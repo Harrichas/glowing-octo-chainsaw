bb91a3e1302c877d99f86f589515613c824ffd92
Chase Harrington <acechaseharrington@gmail.com>
2021-03-27 16:01:14 -0500

    try react google login npm

--- client/src/pages/Oauth.js
+++ client/src/pages/Oauth.js
@@ -1,7 +1,16 @@
 import React from "react";
+import GoogleLogin from "react-google-login";
+import axios from "axios"
 
 function Oauth() {
   return (
+    <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    />,
     <a href="http://localhost:3001/google">login</a>
   );
 }
