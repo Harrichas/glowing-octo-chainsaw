b1f71d97218e6631361d16976d4e9bfd030cd428
Chase Harrington <acechaseharrington@gmail.com>
2021-03-29 19:09:29 -0500

    comment google oauth to fix crash

--- client/src/pages/Oauth.js
+++ client/src/pages/Oauth.js
@@ -4,14 +4,14 @@ import axios from "axios"
 
 function Oauth() {
   return (
-    <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-    />,
-    <a href="http://localhost:3001/google">login</a>
+    // <GoogleLogin
+    // clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    // buttonText="Login"
+    // onSuccess={responseGoogle}
+    // onFailure={responseGoogle}
+    // cookiePolicy={'single_host_origin'}
+    // />,
+    <a href="http://localhost:3001/google">login</a>                                  
   );
 }
 
